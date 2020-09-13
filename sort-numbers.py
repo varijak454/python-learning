77061feefd10148db29669515a6d66b153d97534
varija <varijak454@gmail.com>
2020-09-13 15:49:24 -0600

    Pushing the Number Sorting Python Program

---
+++ sort-numbers.py
@@ -0,0 +1,8 @@
+# Initilitining list of randaom numbers
+numbers = [2,5,1,9]
+# Sorting on Ascending order
+numbers.sort()
+print("Sording Ascending order:", numbers)
+# Sorting on descending order
+numbers.sort(reverse=True)
+print("Sording Descending Order:", numbers)
